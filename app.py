3bc85b2ed4c63741dfb9b5d7a1637fd0a323610f
Ayushi Dubey <119059967+dubeyayushi@users.noreply.github.com>
2023-03-19 03:37:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+@app.route('/scroll', methods=['POST'])
+def scroll():
+    print("Scroll endpoint called")
+    direction = request.form['direction']
+    if direction == 'down':
+        script = 'window.scrollBy(0, 100);'
+    elif direction == 'up':
+        script = 'window.scrollBy(0, -100);'
+    else:
+        return jsonify({'error': 'Invalid direction'})
+
+    return jsonify({'success': True})
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Stress Level of the Day is {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls through request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
